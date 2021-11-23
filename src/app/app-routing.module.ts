c32bd721f365d8036ba274451ac404b36e0fac56
Shivam Sharma <uic.17mca8010@gmail.com>
2021-11-23 10:32:15 +0530

    ssss
    iiii

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewUsersComponent } from './components/view-users/view-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"viewUsers",
+    component:ViewUsersComponent,
+    pathMatch:"full"
+  }
+  ,
+  {
+    path:"",
+    component:HomeComponent,
+    pathMatch:"full"
+  },
+  {
+    path:"addUser",
+    component:AddUserComponent,
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import {MatRadioModule} from '@angular/material/radio';
+import { UserService } from './service/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ViewUsersComponent } from './components/view-users/view-users.component';
+import { HomeComponent } from './components/home/home.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddUserComponent,
+    ViewUsersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule,
+    MatRadioModule,
+    HttpClientModule,
+    MatTableModule,
+    NgbModule,
+    MatSnackBarModule
+
+
   ],
-  providers: [],
+  providers: [UserService,NgbModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
